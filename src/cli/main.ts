4eab759cf92f9b4f369548d3b63f5075165b1a93
Oliver Reinhard <oliver.reinhard@gmail.com>
2025-08-05 22:07:28 +0200

    Add CLI command for simple text generation
    - implement 'generate' CLI command
    - add trivial, string-based generator

--- src/cli/main.ts
+++ src/cli/main.ts
@@ -1,10 +1,11 @@
-import type { Model } from '../language/generated/ast.js';
 import chalk from 'chalk';
 import { Command } from 'commander';
+import type { Model } from '../language/generated/ast.js';
 import { DomainStorytellingLanguageMetaData } from '../language/generated/module.js';
-import { createDomainStorytellingServices } from '../language/domain-storytelling-module.js';
 import { extractAstNode } from './cli-util.js';
 import { generateJavaScript } from './generator.js';
+import { createDomainStorytellingServices } from '../language/domain-storytelling-module.js';
+import { extractDocument } from './cli-util.js';
 import { NodeFileSystem } from 'langium/node';
 import * as url from 'node:url';
 import * as fs from 'node:fs/promises';
@@ -13,6 +14,50 @@ const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 const packagePath = path.resolve(__dirname, '..', '..', 'package.json');
 const packageContent = await fs.readFile(packagePath, 'utf-8');
+
+export default function(): void {
+    const cmd = new Command();
+    cmd.version(JSON.parse(packageContent).version);
+    cmd
+        .command('parseAndValidate')
+        .argument('<file>', 'Source file to parse & validate (ending in ${fileExtensions})')
+        .description('Indicates where a program parses & validates successfully, but produces no output code')
+        .action(parseAndValidate) // we'll need to implement this function
+
+    const fileExtensions = DomainStorytellingLanguageMetaData.fileExtensions.join(', ');
+    cmd
+        .command('generate')
+        .argument('<file>', `source file (possible file extensions: ${fileExtensions})`)
+        .option('-d, --destination <dir>', 'destination directory of generating')
+        .description('generates JavaScript code that prints "Hello, {name}!" for each greeting in a source file')
+        .action(generateAction);
+
+    cmd.parse(process.argv);
+}
+
+/**
+ * Parse and validate a program written in the DomainStorytelling language.
+ * Verifies that no lexer or parser errors occur.
+ * Implicitly also checks for validation errors while extracting the document
+ *
+ * @param fileName Program to validate
+ */
+export const parseAndValidate = async (fileName: string): Promise<void> => {
+    // retrieve the services for our language
+    const services = createDomainStorytellingServices(NodeFileSystem).DomainStorytelling;
+    // extract a document for our program
+    const document = await extractDocument(fileName, services);
+    // extract the parse result details
+    const parseResult = document.parseResult;
+    // verify no lexer, parser, or general diagnostic errors show up
+    if (parseResult.lexerErrors.length === 0 && 
+        parseResult.parserErrors.length === 0
+    ) {
+        console.log(chalk.green(`Parsed and validated ${fileName} successfully!`));
+    } else {
+        console.log(chalk.red(`Failed to parse and validate ${fileName}!`));
+    }
+}
 
 export const generateAction = async (fileName: string, opts: GenerateOptions): Promise<void> => {
     const services = createDomainStorytellingServices(NodeFileSystem).DomainStorytelling;
@@ -23,20 +68,4 @@ export const generateAction = async (fileName: string, opts: GenerateOptions): P
 
 export type GenerateOptions = {
     destination?: string;
-}
-
-export default function(): void {
-    const program = new Command();
-
-    program.version(JSON.parse(packageContent).version);
-
-    const fileExtensions = DomainStorytellingLanguageMetaData.fileExtensions.join(', ');
-    program
-        .command('generate')
-        .argument('<file>', `source file (possible file extensions: ${fileExtensions})`)
-        .option('-d, --destination <dir>', 'destination directory of generating')
-        .description('generates JavaScript code that prints "Hello, {name}!" for each greeting in a source file')
-        .action(generateAction);
-
-    program.parse(process.argv);
 }
